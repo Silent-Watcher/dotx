f0eea5ba14c73f38c4f4f47af42c60b30a1f316d
Silent-Watcher <alitabatabaee20@gmail.com>
2025-08-10 14:51:02 +0330

    fix: remove source map files from the dist dir

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,18 +1,18 @@
-import { defineConfig } from "tsup";
+import { defineConfig } from 'tsup';
 
 export default defineConfig({
-	entry: ["src/*"],
-	format: ["esm"], // Keep ESM format
-	outDir: "dist",
+	entry: ['src/*'],
+	format: ['esm'], // Keep ESM format
+	outDir: 'dist',
 	clean: true,
-	sourcemap: true,
+	sourcemap: false,
 	splitting: false,
 	bundle: true,
 	dts: false,
 	minify: true,
-	external: ["fs/promises", "path", "commander", "@dotenvx/dotenvx"],
+	external: ['fs/promises', 'path', 'commander', '@dotenvx/dotenvx'],
 	loader: {
-		".json": "json", // ⬅ transforms JSON into normal ESM object
+		'.json': 'json', // ⬅ transforms JSON into normal ESM object
 	},
-	outExtension: () => ({ js: ".js" }), // Force .js instead of .mjs
+	outExtension: () => ({ js: '.js' }), // Force .js instead of .mjs
 });
